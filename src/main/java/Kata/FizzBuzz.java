23d60cbab26e5e6b0d0332078cb28ff27f94d6d3
Ruggiero Stello <rsstello@gmail.com>
2023-01-18 13:05:43 -0300

    passou teste Fizz

--- src/main/java/Kata/FizzBuzz.java
+++ src/main/java/Kata/FizzBuzz.java
@@ -2,7 +2,11 @@ package Kata;
 
 public class FizzBuzz {
     public String convert(int toConvertToFizzBuzz) {
-        return "1";
+        if(toConvertToFizzBuzz%3==0){
+            return "Fizz";
+        }
+
+        return String.valueOf(toConvertToFizzBuzz);
     }
 
 }
