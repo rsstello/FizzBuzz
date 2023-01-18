86190deee7060496bf9c656a005332cf5d0afc87
Ruggiero Stello <rsstello@gmail.com>
2023-01-18 13:09:25 -0300

    Update FizzBuzz.java

--- src/main/java/Kata/FizzBuzz.java
+++ src/main/java/Kata/FizzBuzz.java
@@ -5,7 +5,9 @@ public class FizzBuzz {
         if(toConvertToFizzBuzz%3==0){
             return "Fizz";
         }
-
+        if(toConvertToFizzBuzz%5==0){
+            return "Buzz";
+        }
         return String.valueOf(toConvertToFizzBuzz);
     }
 
