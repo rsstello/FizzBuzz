9ed08c3b20716ff7606ce5b75a24feeb766ea35f
Ruggiero Stello <rsstello@gmail.com>
2023-01-18 12:37:25 -0300

    second test fail

--- src/test/java/Kata/FizzBuzzTest.java
+++ src/test/java/Kata/FizzBuzzTest.java
@@ -6,13 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     void testFizzBuzzConverterNoChange () {
 
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         final String esperado = "1";
         final String obtido = fizzBuzz.convert(1);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testFizzBuzzConverterMultipleOfThree () {
+
+        final String esperado = "Fizz";
+        final String obtido = fizzBuzz.convert(3);
         assertEquals(esperado, obtido);
     }
 
